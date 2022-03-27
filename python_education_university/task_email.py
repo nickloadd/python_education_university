225d01f8b67308d33a4ce37f2d2c203a9a309010
nickloadd <32330419+nickloadd@users.noreply.github.com>
2022-03-27 19:10:39 +0700

    get valid list of email address

--- python_education_university/task_email.py
+++ python_education_university/task_email.py
@@ -1,16 +1,35 @@
-f = open('C:/Users/admin/PycharmProjects/repos/task.txt')
+f = open('C:/Users/admin/PycharmProjects/repos/task_file.txt') #read info from file
 s = []
-z = []
+q = []
 list = []
-for line in f:
+work_list = []
+for line in f: #parsing file in new list
     z = []
     s = line.split(",")
-    z.append(s[1].strip())
-    z.append(s[2].strip())
+    for i in s:
+        z.append(i)
     list.append(z)
+#list.pop(0)
+#print(list)
 
+for i in list[1:]: #loop for get list of value without empty field and wrong phone number
+    for k in range(1,5):
+        if (len(i[k].strip()) == 0) or (len(i[3].strip()) != 7 ):
+            #print(i)
+            list.remove(i)
+            break
+        else:
+            continue
+#print(list)
 
-def email_gen(list_of_names):
+for k in list[1:]: #get valid list of Name + Last_Name
+    work_list = []
+    work_list.append(k[1].strip())
+    work_list.append(k[2].strip())
+    q.append(work_list)
+#print(q)
+
+def email_gen(list_of_names): # function generation email address for valid list
     emails = []
     for i in list_of_names:
         letter = 1
@@ -20,4 +39,4 @@ def email_gen(list_of_names):
     return emails
 
 
-print(email_gen(list))
+print(email_gen(q))
